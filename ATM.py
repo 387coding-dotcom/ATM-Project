4eed453199b10b09809b3383761230426a7715b2
Shravani Bhoir <387coding@gmail.com>
2026-09-12 23:10:53 +0530

    added conditions and choices

---
+++ ATM.py
@@ -0,0 +1,60 @@
+#Creating the list and variables
+balance= 1000
+cor_pin= 3807
+print("---------ATM Management System-----------")
+pin=int(input("Enter Your PIN: "))
+#Adding conhdition to lock account after three unhsuccessful attempts
+if pin==cor_pin:
+    print("\n Login Successful!")
+
+    while True:
+        print("\n----ATM MENU----")
+        print("1. Check Balance") 
+        print("2. Deposit Amount") 
+        print("3. Withdraw Amount") 
+        print("4. Transaction History") 
+        print("5. Logout") 
+        
+        choice=int(input("Enter Your Choice: "))
+        
+        if choice == 1:
+            print("Your balance is: ",balance)
+        
+        elif choice == 2:
+            amount1=float(input("Enter the amount to deposit: "))
+            
+            if amount1> 0:
+                balance+=amount1
+                print("Amount deposited successfully.")
+                print("Updated balance:", balance)
+            else:
+                print("Invalid Amount")
+            
+        elif choice == 3:
+            amount2=float(input("Enter the amount to be credited: "))
+            
+            if amount2>balance:
+                print("Insufficient Balance")
+            else:
+                balance-=amount2
+                print("Amount Witdrawn Successfully!")
+                print("Updated Balance", balance)
+                
+        elif choice == 4:
+         print("--Transaction History---------")
+         print("Amount deposited",amount1)
+         print("Amount Witdrawn", amount2)
+         print("Your balance is: ",balance)
+         print("Thanhk You")
+        
+        elif choice == 5:
+            print("Thank you for working with our bank")
+            break
+            
+        else:
+            print("Invalid Choice")
+          
+else:
+    print("Invalide PIN")
+            
+            
